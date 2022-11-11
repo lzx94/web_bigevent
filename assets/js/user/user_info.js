5bf7db12e7e3922da2f956572b1f96cff966d1a0
lzx <278054514@qq.com>
2022-11-11 14:13:31 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,53 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间!'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    function initUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'GET',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败!')
+                }
+                form.val('formUserInfo', res.data);
+            }
+
+        })
+    }
+
+    $('#btnReset').on('click', function(e) {
+        e.preventDefault();
+        initUserInfo();
+    })
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败!')
+                }
+                layer.msg('更新用户信息成功!')
+                window.parent.getUserInfo();
+            }
+        })
+    })
+
+
+
+
+})
